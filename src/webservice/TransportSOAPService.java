9dc711aeef303e52522c72cb9efb35aeba433509
izardj <izard.jerome@free.fr>
2017-05-05 16:33:27 +0200

    Enleve interface Local et Remote car non utilisees

--- src/webservice/TransportSOAPService.java
+++ src/webservice/TransportSOAPService.java
@@ -17,37 +17,35 @@ import metier.Marchandise;
  */
 @WebService(serviceName = "Transport")
 @Stateless
-public class TransportSOAPService implements TransportSOAPServiceRemote, TransportSOAPServiceLocal {
+public class TransportSOAPService {
 
 	@EJB
 	private ITransportLocal dao;
-	
+
 	@WebMethod
-	@Override
-	public void addCargaison(@WebParam(name="cargaison") Cargaison c) {
+	public void addCargaison(@WebParam(name = "cargaison") Cargaison c) {
 		dao.addCargaison(c);
 	}
 
 	@WebMethod
-	@Override
-	public void addMarchandise(@WebParam(name="marchandise") Marchandise m, @WebParam(name="refCargaison") long refCargaison) {
-		dao.addMarchandise(m, refCargaison);		
+	public void addMarchandise(@WebParam(name = "marchandise") Marchandise m,
+			@WebParam(name = "refCargaison") long refCargaison) {
+		dao.addMarchandise(m, refCargaison);
 	}
 
 	@WebMethod
-	@Override
 	public List<Cargaison> getAllCargaisons() {
 		return dao.getAllCargaisons();
 	}
 
-	@Override
-	public List<Marchandise> getMarchandisesParMC(@WebParam(name="mc") String mc) {
+	@WebMethod
+	public List<Marchandise> getMarchandisesParMC(@WebParam(name = "mc") String mc) {
 		return dao.getMarchandisesParMC(mc);
 	}
 
-	@Override
-	public void deleteMarchandise(@WebParam(name="refMarchandise") long refMarchandise) {
-		dao.deleteMarchandise(refMarchandise);		
+	@WebMethod
+	public void deleteMarchandise(@WebParam(name = "refMarchandise") long refMarchandise) {
+		dao.deleteMarchandise(refMarchandise);
 	}
 
 }
